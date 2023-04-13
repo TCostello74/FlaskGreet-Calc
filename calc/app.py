2925176a9fb8fcbdda46842190348e9f9c837016
tcostello74 <tcostello74@yahoo.com>
2023-04-13 18:40:35 -0400

    calc changes

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,61 @@
 # Put your app in here.
+from flask import Flask, request
+
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add_nums():
+    """will add a and b params"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+
+    return str(result)
+
+
+@app.route('/sub')
+def sub_nums():
+    """will subtract a and b params"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+
+    return str(result)
+
+
+@app.route('/mult')
+def mult_nums():
+    """will multiply a and b params"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+
+    return str(result)
+
+
+@app.route('/div')
+def div_nums():
+    """will divide a and b params"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+
+    return str(result)
+
+operators = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div,
+}
+
+@app.route("/math/<oper>")
+def do_math(oper):
+    """will do math on params"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operators[oper](a, b)
+
+    return str(result)
